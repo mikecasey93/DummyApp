cffdcd51d459890a9734a289002f7148255e3aa9
michaelcasey <caseymichaelt1893@gmail.com>
2024-05-29 11:49:26 -0400

    Code from second branch

--- app/src/main/java/com/example/dummyapp/MainActivity.kt
+++ app/src/main/java/com/example/dummyapp/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     DummyAppTheme {
-        Greeting("Android"
+        Greeting("Greetings Android Users")
     }
 }
